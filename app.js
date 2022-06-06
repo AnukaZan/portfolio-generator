fc5297bcf5fa01c22ed63b786350e7a0c660ea62
Anuka <anukazan@gmail.com>
2022-06-05 19:48:05 -0700

    add inquirer

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const fs = require('fs');
-
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2); //save command text after 3 indeces
-
-const [name, github] = profileDataArgs; //assign elements of array to variable names 
+const inquirer = require('inquirer');
 
 
-fs.writeFile('index.html', generatePage(name, github), err=>{ //1st file name, 2nd output file, 3rd handling errors
-    if (err) throw new Error (err); //if error then stop execution of code
+// const fs = require('fs');
 
-    console.log('Portfolio complete! Check out the index.html to see the output!');
-});
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('index.html', pageHTML, err=>{ //1st file name, 2nd output file, 3rd handling errors
+//     if (err) throw err; //if error then stop execution of code
+
+//     console.log('Portfolio complete! Check out the index.html to see the output!');
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
